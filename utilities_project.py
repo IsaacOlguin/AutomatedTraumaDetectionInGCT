3d0969074929fa8cf412aa887f6928a71a48e66d
IsaacOlguin <misael.olguin@gmail.com>
2022-11-11 02:13:14 +0100

    Analysis of topics top2vec, execution of segmenters on example. Modif. in utilities to include regex to remove URLs

--- utilities_project.py
+++ utilities_project.py
@@ -1,6 +1,7 @@
-from google.colab import drive
 import pandas as pd
 from enum import Enum
+import string
+import re
 
 import nltk
 from nltk.corpus import words
@@ -8,7 +9,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 
-#nltk.download('stopwords')
+nltk.download('stopwords')
+nltk.download()
 
 ROOT_DRIVE_PATH = '/content/drive'
 list_stop_words_english = stopwords.words("english")
@@ -20,10 +22,12 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 #= Connection is established on the root path of Drive
 def drive_connect():
+    from google.colab import drive
     drive.mount(ROOT_DRIVE_PATH)
 
 #= Connection is established on the specified path
 def drive_connect_to_path(path):
+    from google.colab import drive
     if path == "":
         drive_connect()
     else:
@@ -50,6 +54,7 @@ class PreprocessingText(Enum):
     STOP_WORDS = 5
     STEMMING = 6
     LEMMATIZATION = 7
+    REMOVE_URL = 8
     
     
 #################################################################################
@@ -59,29 +64,37 @@ class PreprocessingText(Enum):
 
 def pre_processing_sentence(sentence_tokens, list_preprocessing_steps):
     if (type(list_preprocessing_steps) != 'list'):
-        list_preprocessing_steps = [PreprocessingText.LOWER_CASE, PreprocessingText.PUNCT_REGEX, PreprocessingText.PUNCTUATION, PreprocessingText.NUMBERS, PreprocessingText.STOP_WORDS, PreprocessingText.STEMMING, PreprocessingText.LEMMATIZATION]
+        list_preprocessing_steps = [PreprocessingText.LOWER_CASE, PreprocessingText.PUNCT_REGEX, PreprocessingText.PUNCTUATION, PreprocessingText.NUMBERS, PreprocessingText.STOP_WORDS, PreprocessingText.STEMMING, PreprocessingText.LEMMATIZATION, PreprocessingText.REMOVE_URL]
     
     
     # LowerCase
     if PreprocessingText.LOWER_CASE in list_preprocessing_steps:
         new_sentence = sentence_tokens.lower()
-        print("I applied Lowercase")
+
+    # Remove URLs
+    if PreprocessingText.REMOVE_URL in list_preprocessing_steps:
+        new_sentence = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', new_sentence)
     
     # Tokenization with RegEx
-    new_sentence = ' '.join(tokenizer.tokenize(new_sentence))
+    if PreprocessingText.PUNCT_REGEX in list_preprocessing_steps:
+        new_sentence = ' '.join(tokenizer.tokenize(new_sentence))
     
     # Remove punctuation
-    new_sentence = ''.join([character for character in new_sentence if character not in string.punctuation])
+    if PreprocessingText.PUNCTUATION in list_preprocessing_steps:
+        new_sentence = ''.join([character for character in new_sentence if character not in string.punctuation])
     
     # Remove numbers
-    new_sentence = ''.join([ character for character in new_sentence if not character.isdigit() ])
+    if PreprocessingText.NUMBERS in list_preprocessing_steps:
+        new_sentence = ''.join([ character for character in new_sentence if not character.isdigit() ])
     
     # Remove stopwords
-    new_sentence = [ token for token in new_sentence.split() if token not in list_stop_words_english ]
-    new_sentence = ' '.join(new_sentence)
+    if PreprocessingText.STOP_WORDS in list_preprocessing_steps:
+        new_sentence = [ token for token in new_sentence.split() if token not in list_stop_words_english ]
+        new_sentence = ' '.join(new_sentence)
     
     # Remove lemmas
-    new_sentence = [ stemmer.lemmatize(token) for token in new_sentence.split() ]
-    new_sentence = ' '.join(new_sentence)
+    if PreprocessingText.LEMMATIZATION in list_preprocessing_steps:
+        new_sentence = [ stemmer.lemmatize(token) for token in new_sentence.split() ]
+        new_sentence = ' '.join(new_sentence)
     
     return new_sentence
