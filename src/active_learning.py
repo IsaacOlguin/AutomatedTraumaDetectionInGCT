897860967694d0e791075de1a034fa790e80898c
IsaacOlguin <misael.olguin@gmail.com>
2023-01-30 18:49:50 +0000

    Modification of active learning src for the training, validation and test execution

--- src/active_learning.py
+++ src/active_learning.py
@@ -3,7 +3,8 @@
 ###################################################################################################
 @author:    Isaac Misael Olguín Nolasco
 @project:   Automated Trauma Detection in Genocide Court Transcripts (IDP - TUM Informatics)
-@file:      Contains the implementation for the active learning process.
+@file:      active_learning.py
+@descrip:   Contains the implementation for the active learning process.
 ###################################################################################################
 ###################################################################################################
 """
@@ -59,7 +60,7 @@ from sklearn.exceptions import UndefinedMetricWarning
 import warnings
 # Custom code
 import classification_model_utilities as mlclassif_utilities
-
+import general_utilities as gral_utilities
 
 ###################################################################################################
 ###################################################################################################
@@ -84,7 +85,7 @@ def configure_logger(levelStdout=logging.DEBUG, levelFile=logging.DEBUG):
     stdout_handler.setFormatter(formatter)
     """
     
-    file_handler = logging.FileHandler(join(PATH_PROJECT, PATH_DIR_LOGS, get_datetime_format() + '_activeLearning.log'))
+    file_handler = logging.FileHandler(join(PATH_PROJECT, PATH_DIR_LOGS, gral_utilities.get_datetime_format() + '_activeLearning.log'))
     
     file_handler.setLevel(levelFile)
     file_handler.setFormatter(formatter)
@@ -150,6 +151,8 @@ def read_config_file(config_file_path):
     if config_file_path == None:
         infoLog("Path of the configuration file is not defined. It's considered a default value ['config.yml']")
         config_file_path = "config.yml"
+    
+    cfg = None
     
     with open(join(PATH_PROJECT, config_file_path), READ_FILE_MODE) as ymlfile:
         cfg = yaml.safe_load(ymlfile)
@@ -224,6 +227,8 @@ def read_config_file(config_file_path):
 
         global GLB_MODEL_NAME
         GLB_MODEL_NAME = cfg["training_model"]["model_name"]
+        
+    return cfg
 
 """
 Function:       read_input_arguments()
@@ -232,182 +237,6 @@ Return:         List - of input arguments
 """  
 def read_input_arguments():
     return sys.argv
-
-"""
-Function:       get_datetime_format()
-Description:    Get the date and time with the format for storing files
-Return:         String - Date and time
-"""
-def get_datetime_format():
-    return dt.datetime.now().strftime("%Y%m%d%H%M%S")
-    
-###################################################################################################
-###################################################################################################
-### Functions for training
-###################################################################################################
-###################################################################################################
-"""
-Function:       train(dataframe)
-Description:    Execute training
-Return:         Tuple - (model, metrics)
-""" 
-def train(df_dataset, num_classes):
-    # Get classes of the dataset
-    classes_dataset = mlclassif_utilities.get_unique_values_from_dataset(df_dataset, COL_OF_INTEREST)
-    debugLog(f"Num of different classes in the dataset is {len(classes_dataset)} which are:")
-    for index, elem in enumerate(classes_dataset):
-        debugLog(f"\t {index+1} - {elem}")
-    
-    # Define device to be used
-    device = mlclassif_utilities.get_gpu_device_if_exists()
-    debugLog(f"\n\n==> Selected device is '{device}' <==")
-    
-    #If no parameters are sent, default values are considered. 
-    #    IDModel:      Bert
-    #    Model namel:  bert-base-uncased
-    #    Do uncase:    True
-    
-    # Get tokenizer
-    tokenizer = mlclassif_utilities.get_tokenizer() 
-
-    # Get lists of spans and classes
-    list_all_spans = list(df_dataset[COL_OF_REFERENCE])
-    list_all_classes = [int(elem) if isfloat(elem) else elem for elem in df_dataset[COL_OF_INTEREST]]
-    
-    # Get & print the sentence and length of the largest one
-    mlclassif_utilities.get_max_length_of_a_sentence_among_all_sentences(tokenizer, list_all_spans, False)
-    
-    # If _return_attention_mask, a tuple of two lists is given (tensor_of_inputs, tensor_of_attention_masks)
-    all_spans_tokenized = mlclassif_utilities.get_all_spans_tokenized(
-        mlclassif_utilities.GLB_BERT_MODEL_ID, 
-        tokenizer,
-        list_all_spans,
-        _add_special_tokens = GLB_ADD_SPECIAL_TOKENS, 
-        _max_length = GLB_MAX_LENGTH_SENTENCE,
-        _pad_to_max_length = GLB_PADDING_TO_MAX_LENGTH,
-        _return_attention_mask = GLB_RETURN_ATTENTION_MASK, 
-        type_tensors = mlclassif_utilities.GLB_PYTORCH_TENSOR_TYPE
-    )
-    
-    input_ids = None
-    attention_masks = None
-    
-    if GLB_RETURN_ATTENTION_MASK:
-        input_ids = mlclassif_utilities.convert_list_into_pytorch_tensor(all_spans_tokenized[0])
-        attention_masks = mlclassif_utilities.convert_list_into_pytorch_tensor(all_spans_tokenized[1])
-    else:
-        input_ids = mlclassif_utilities.convert_list_into_pytorch_tensor(all_spans_tokenized)
-    
-    numeric_classes = mlclassif_utilities.convert_list_span_classes_into_numeric_values(classes_dataset, list_all_classes)
-    numeric_classes = mlclassif_utilities.convert_list_labels_into_pytorch_tensor(numeric_classes)
-    
-    ### Split dataset
-    if not GLB_CROSS_VALIDATION:
-        train_labels_corpus, train_input_ids, train_attention_masks, val_labels_corpus, val_input_ids, val_attention_masks, test_labels_corpus, test_input_ids, test_attention_masks = mlclassif_utilities.split_dataset_train_val_test(numeric_classes, input_ids, attention_masks)
-    else:
-        ### k-Fold
-        train_val_corpus_cross_validation, test_corpus_cross_validation = mlclassif_utilities.split_dataset_train_val_test_k_fold(numeric_classes, input_ids, attention_masks, 0.1)
-        
-    ### Create model
-    model = mlclassif_utilities.create_model(
-        mlclassif_utilities.GLB_BERT_MODEL_ID,
-        mlclassif_utilities.GLB_BERT_BASE_UNCASED_MODEL_NAME,
-        num_classes,
-        GLB_RUN_IN_GPU #RunInGPU
-    )
-    
-    # Define optimizer
-    optimizer = mlclassif_utilities.get_optimizer(model)
-    
-    # Define scheduler
-    scheduler = mlclassif_utilities.get_scheduler(optimizer)
-    
-    if not GLB_CROSS_VALIDATION:
-        train_dataset = mlclassif_utilities.create_tensor_dataset(train_input_ids, train_attention_masks, train_labels_corpus)
-        val_dataset = mlclassif_utilities.create_tensor_dataset(val_input_ids, val_attention_masks, val_labels_corpus)
-        test_dataset = mlclassif_utilities.create_tensor_dataset(test_input_ids, test_attention_masks, test_labels_corpus)
-        
-        train_dataloader = mlclassif_utilities.create_dataloader(train_dataset, BATCH_SIZE)
-        val_dataloader = mlclassif_utilities.create_dataloader(val_dataset, BATCH_SIZE)
-        test_dataloader = mlclassif_utilities.create_dataloader(test_dataset, BATCH_SIZE)
-        
-        model, statistics_model = mlclassif_utilities.train_and_validate(model, device, EPOCHS, optimizer, scheduler, train_dataloader, val_dataloader, numeric_classes.tolist())
-    
-    else:
-        list_statistics = list()
-        for index_cross_val in range(len(train_val_corpus_cross_validation)):
-            train_dataset = mlclassif_utilities.create_tensor_dataset(train_val_corpus_cross_validation[index_cross_val][1], train_val_corpus_cross_validation[index_cross_val][2], train_val_corpus_cross_validation[index_cross_val][0])
-            val_dataset = mlclassif_utilities.create_tensor_dataset(train_val_corpus_cross_validation[index_cross_val][4], train_val_corpus_cross_validation[index_cross_val][5], train_val_corpus_cross_validation[index_cross_val][3])
-    
-            train_dataloader = mlclassif_utilities.create_dataloader(train_dataset, BATCH_SIZE)
-            val_dataloader = mlclassif_utilities.create_dataloader(val_dataset, BATCH_SIZE)
-    
-            debugLog('='*50)
-            debugLog(f"Cross-Validation Split {(index_cross_val+1)}/{len(train_val_corpus_cross_validation)}")
-            debugLog('='*50)
-            model, statistics_model = mlclassif_utilities.train_and_validate(model, device, EPOCHS, optimizer, scheduler, train_dataloader, val_dataloader, numeric_classes.tolist())
-            list_statistics.append(statistics_model)
-        
-        if GLB_STORE_STATISTICS_MODEL:
-            mlclassif_utilities.save_json_file_statistics_model({"cross-validation": list_statistics}, PATH_DIR_LOGS)
-        
-    return model, statistics_model
-    
-###################################################################################################
-###################################################################################################
-### Functions for active training
-###################################################################################################
-###################################################################################################
-
-def give_me_segments_of_df_per_class(df, number_of_splits, column_of_interest, column_of_reference):
-    dict_of_segments = {}
-    invalidSplit = False
-    number_of_classes = df[column_of_interest].nunique()
-    list_of_classes = df[column_of_interest].unique()
-    
-    counts = df[column_of_interest].value_counts()
-    normalized = round(df[column_of_interest].value_counts(normalize=True), 4)
-    percentages = normalized*100
-    
-    df_stats_dataset = pd.DataFrame({'counts': counts, 'normalized': normalized, 'percentages': percentages}).reset_index()
-    
-    # Validation
-    for i, row in df_stats_dataset.iterrows():
-        if row["counts"] < number_of_splits:
-            errorLog(f"ERROR - Dataset[{row['index']}] cannot be split into the given number of splits")
-            invalidSplit = True
-    
-    if invalidSplit:
-        return None
-    else:
-        # Get sizes of segments and put them into a list
-        list_of_size_segments = (df_stats_dataset["counts"]-(df_stats_dataset["counts"]%number_of_splits)) / number_of_splits
-        
-        """
-        print("*"*100)
-        print(df_stats_dataset)
-        print("*"*100)
-        """
-        
-        # Initialize dict_of_segments
-        for i_range in range(0, number_of_splits):
-            dict_of_segments[i_range] = pd.DataFrame()
-        
-        # Add segments to a list of segments
-        for index_class, (size, type_id) in enumerate(zip(list_of_size_segments, df_stats_dataset["index"])):
-            size = int(size)
-            #print(index_class, "#"*100, size)
-            for i_range in range(0, number_of_splits):
-                #print(i_range, "*"*50, index_class, type_id, "Segment", i_range, "[", i_range*size, ":", i_range*size+size, "]")
-                if index_class == 0:
-                    dict_of_segments[i_range] = df[df[COL_OF_INTEREST] == type_id][i_range*size:i_range*size+size]
-                else:
-                    if (i_range+1) == number_of_splits:
-                        dict_of_segments[i_range] = pd.concat([dict_of_segments[i_range], df[df[COL_OF_INTEREST] == type_id][i_range*size:]])
-                    else:
-                        dict_of_segments[i_range] = pd.concat([dict_of_segments[i_range], df[df[COL_OF_INTEREST] == type_id][i_range*size:i_range*size+size]])
-    
-    return dict_of_segments
 
 ###################################################################################################
 ###################################################################################################
@@ -425,21 +254,27 @@ def main():
     debugLog("Reading directory path")
     get_projects_directory_path()
     # Read configuration file
-    read_config_file("config.yml")
+    cfg = read_config_file("config.yml")
     
     # Get input arguments
     #input_arguments = read_input_arguments()
     #print(f"The number of input arguments is [{len(input_arguments)}] whose content is {input_arguments}")
     
     df_dataset = mlclassif_utilities.import_dataset_from_excel(PATH_DATASET, INDEX_COLUMNS_DATASET, LIST_NAME_COLUMNS_DATASET)
-    print(f"Col of interest {COL_OF_INTEREST}")
+    debugLog(f"Col of interest {COL_OF_INTEREST}")
     classes_dataset = [int(elem) if isfloat(elem) else elem for elem in mlclassif_utilities.get_unique_values_from_dataset(df_dataset, COL_OF_INTEREST)]
-    print(f"classes_dataset {classes_dataset}")
+    debugLog(f"classes_dataset {classes_dataset}")
     
     # Split the dataset for the Active Training purposes
-    dict_of_segments = give_me_segments_of_df_per_class(df_dataset, GLB_SIZE_SPLITS_DATASET, COL_OF_INTEREST, COL_OF_REFERENCE)
+    dict_of_segments = mlclassif_utilities.give_me_segments_of_df_per_class(df_dataset, GLB_SIZE_SPLITS_DATASET, COL_OF_INTEREST, COL_OF_REFERENCE)
     odlst = collections.OrderedDict(sorted(dict_of_segments.items()))
     list_statistics = list()
+    
+    GLB_ID_MODEL = mlclassif_utilities.get_id_model(cfg, GLB_MODEL_NAME)
+    if GLB_ID_MODEL == None or GLB_ID_MODEL == "":
+        infoLog("ID of the model was not found. Execution is finished.")
+        return
+    
     for index, df_at_index in odlst.items():
         if index == 0:
             df = dict_of_segments[index]
@@ -447,24 +282,49 @@ def main():
             df = pd.concat([df, dict_of_segments[index]])
         
         # Read config file again - in case it is desired to store a model or change something at execution time
-        read_config_file("config.yml")
+        cfg = read_config_file("config.yml")
         
-        model, statistics = train(df, len(classes_dataset))
+        model, statistics, test_corpus = mlclassif_utilities.exec_train(
+                                        df, 
+                                        COL_OF_INTEREST, 
+                                        COL_OF_REFERENCE, 
+                                        GLB_ID_MODEL, 
+                                        GLB_MODEL_NAME, 
+                                        batch_size = cfg["training_model"]["batch_size"],
+                                        epochs = cfg["training_model"]["epochs"],
+                                        max_length_sentence = cfg["training_model"]["max_length"],
+                                        cross_validation = cfg["training_model"]["cross_validation"],
+                                        store_statistics_model = cfg["training_model"]["store_statistics"]
+                                    )
         debugLog("="*100)
         debugLog("*"*80)
         debugLog(f"Iteration: {index+1}/{GLB_SIZE_SPLITS_DATASET}")
         infoLog(statistics)
         debugLog("*"*80)
         debugLog("="*100)
-        list_statistics.append(statistics)
         
+        
+        testing_stats = None
+        if cfg["training_model"]["test_model"]:
+            infoLog("To execute testing of the model on test_data")
+            device = mlclassif_utilities.get_gpu_device_if_exists()
+            
+            test_dataset = mlclassif_utilities.create_tensor_dataset(test_corpus[1], test_corpus[2], test_corpus[0])
+            test_dataloader = mlclassif_utilities.create_dataloader(test_dataset, cfg["training_model"]["batch_size"])
+            
+            testing_stats = mlclassif_utilities.test_model(model, device, test_dataloader)
+            
+            aux_json = {"training_and_val": statistics, "test": testing_stats}
+            list_statistics.append(f"{aux_json}")
+        else:
+            list_statistics.append(statistics)
+        
+        
+    infoLog(f"From active learning we present the following statistics: {statistics}")
     if GLB_STORE_STATISTICS_MODEL:
-        mlclassif_utilities.save_json_file_statistics_model(list_statistics, PATH_DIR_LOGS)
+        mlclassif_utilities.save_json_file_statistics_model(f"active_learning: {list_statistics}", PATH_DIR_LOGS,
+            pattern=f'{GLB_MODEL_NAME}-active_learning-{CLASSIFICATION_TASK}-epochs{cfg["training_model"]["epochs"]}-batchSize{cfg["training_model"]["batch_size"]}')
 
-    """
-    if GLB_TEST_MODEL:
-        mlclassif_utilities.test_model(model, device, test_dataloader, numeric_classes.tolist())
-    """
         
     if GLB_SAVE_MODEL:
         mlclassif_utilities.save_model(model, f"model_active_lrn_{CLASSIFICATION_TASK}", PATH_DIR_MODELS)
