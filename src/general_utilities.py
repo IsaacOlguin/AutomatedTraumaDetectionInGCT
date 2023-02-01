372c45601f1fe9480f121f8c880421eefd9977ff
IsaacOlguin <misael.olguin@gmail.com>
2023-02-01 03:59:43 +0100

    Addition of function for printing tree structure of a JSON file

--- src/general_utilities.py
+++ src/general_utilities.py
@@ -3,6 +3,7 @@ import logging
 import os
 from os.path import join
 import yaml
+import json
 
 ##==========================================================================================================
 """
@@ -57,7 +58,7 @@ def configure_logger(levelStdout=logging.DEBUG, levelFile=logging.DEBUG, path_pr
 ##==========================================================================================================
 """
 Function:       read_config_file()
-Description:    Configure logger of the project
+Description:    Read configuration file
 Parameters:     config_file_path - path to the config file
 Return:         Either:
                 - cfg   - the configuration JSON content
@@ -72,3 +73,77 @@ def read_config_file(config_file_path):
         return cfg
 
     return None
+
+##==========================================================================================================
+"""
+Function:       read_json_file(path_json_file)
+Description:    Configure logger of the project
+Parameters:     config_file_path - path to the config file
+Return:         Either:
+                - cfg   - the configuration JSON content
+                - None  - In case, there was a problem while opening/reading the file
+"""
+def read_json_file(path_json_file):
+    json_object = None
+    try:
+        json_object = json.load(open(path_json_file))
+
+        if(type(json_object) == str):
+            json_object = json.loads(json_object)
+
+    except Exception as err:
+        print("ERROR - Error while reading the JSON file")
+        print(err)
+        json_object = None
+    return json_object
+        
+##==========================================================================================================
+"""
+Function:       print_structure_json(json_object)
+Description:    Go through the structure of the JSON object and print key and datatype
+Parameters:     json_object - Refers to a JSON object
+Return:         None
+"""
+def print_structure_json(json_object, level=0, identifier="", limit_depth=1000):
+    #print(json_object, level, type(json_object))
+    if level == 0:
+        if type(json_object) != dict:
+            print("Error - Object received is not a Dict Object", type(json_object))
+            return None
+
+    if limit_depth <= level:
+        return None
+
+    sep = "\t"*level
+    if len(identifier) == 0:
+        print(f'{sep}{type(json_object)}')
+    else:
+        print(f'{sep}{identifier} - {type(json_object)}')
+    
+    if type(json_object) == dict:            
+        for key in json_object.keys():
+            if type(json_object[key]) == dict or type(json_object[key]) == list:
+                print_structure_json(json_object[key], level=level+1, identifier=key, limit_depth=limit_depth)
+            else:
+                if limit_depth <= (level+1):
+                    continue
+
+                aux_sep = "\t"*(level+1)
+                print(f'{aux_sep}{key} {type(json_object[key])}')
+    elif type(json_object) == list:
+        """
+        sep = "\t"*level
+        if len(identifier) == 0:
+            print(f'{sep}{type(json_object)}')
+        else:
+            print(f'{sep}{identifier} - {type(json_object)}')
+        """
+        for index_elem, elem in enumerate(json_object):
+            if type(elem) == dict or type(elem) == list:
+                print_structure_json(elem, level=level+1, identifier=f'{index_elem}', limit_depth=limit_depth)
+            else:
+                aux_sep = "\t"*(level+1)
+                print(f'{aux_sep}{type(elem)}')
+    else:
+        aux_sep = "\t"*(level+1)
+        print(f'{aux_sep}{identifier} - {type(json_object)}')
